6039a9251228ccb6ae989c97d4b22a721b09e44c
saint-lag <maiasena.gabriel@gmail.com>
2022-09-12 16:15:54 -0300

    [PASS] Req 12

--- src/services/post.js
+++ src/services/post.js
@@ -4,21 +4,38 @@ const {
   // User,
   BlogPost,
   PostCategory,
+  Category,
 } = require('../database/models');
 
 const sequelize = new Sequelize(config.development);
 
+const MIN_QUERY_LENGTH = 0;
+
+const categoryIdFilter = async (categoryIds) => {
+ const promise = await Promise.all(categoryIds.map(async (categoryId) => {
+      const query = await Category.findAll({ where: { id: categoryId } });
+      return query.length > MIN_QUERY_LENGTH && categoryId;
+    }));
+  return promise.filter((item) => typeof item === 'number');
+};
+
+const postCategoriesFormatter = (filteredCategoryIds, id) => filteredCategoryIds.map(
+  (categoryId) => ({
+    postId: id,
+    categoryId,
+  }),
+);
+
 const createPost = async ({ title, content, categoryIds }, user) => {
   const userId = user.id;
+  const filteredCategoryIds = await categoryIdFilter(categoryIds);
   try {
     const data = await sequelize.transaction(async (t) => {
       const { id, updated, published } = await BlogPost.create(
         { title, content, userId },
         { transaction: t },
       );
-      console.log('UPDATED, PUBLISHED');
-      console.log(updated, published);
-      const postCategories = categoryIds.map((categoryId) => ({ postId: id, categoryId }));
+      const postCategories = postCategoriesFormatter(filteredCategoryIds, id);
       await PostCategory.bulkCreate(postCategories, { transaction: t });
 
       return { id, title, content, userId, updated, published };
